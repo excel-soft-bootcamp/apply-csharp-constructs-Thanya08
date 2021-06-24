4de2dd947b211c5df4e27dd1707a6f918ead5d70
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-24 20:02:21 +0530

    Add files via upload

---
+++ BMICalFunc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class Bmicalfunction
+
+    {
+        public static Double meter, kg, BMI;
+        public static void calcBMI(double meter, double kg)
+        {
+            BMI = kg / meter;
+            if (BMI < 18.5) { Console.WriteLine("underweight"); }
+            else if (BMI >= 25) { Console.WriteLine("overwieght"); }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter height");
+            meter = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter weight");
+            kg = Convert.ToDouble(Console.ReadLine());
+            calcBMI(meter, kg);
+        }
+    }
+}
+
