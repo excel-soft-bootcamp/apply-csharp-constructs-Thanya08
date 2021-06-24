5c95b4830203d390a5516c860875bc4f1bf3da36
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-24 17:29:47 +0530

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        
+
+
+        static void Main(string[] args)
+        {
+           
+            double meter,kg,BMI;
+
+            Console.WriteLine("Enter height");
+            meter = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter weight");
+            kg = Convert.ToSingle(Console.ReadLine());
+            BMI = kg / meter;
+            if (BMI < 18.5) { Console.WriteLine("overweight"); }
+            else if (BMI >= 25) { Console.WriteLine("healthy"); }
+            
+
+        }
+    }
+}
