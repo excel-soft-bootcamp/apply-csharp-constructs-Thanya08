d414feeed927461b068a8b1e689d65d7c4f8430b
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-25 20:18:52 +0530

    Add files via upload

---
+++ BMICalculatorApp/InputReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorApp
+{
+    class InputReader
+    {
+        public double GetHeight()
+        {
+            double height = double.Parse((this.ReadFromConsole("Enter height"));
+            return height;
+        }
+        public double GetWeight()
+        {
+            double weight = double.Parse((this.ReadFromConsole("Enter Weight"));
+            return weight;
+        }
+    }
+}
---
+++ BMICalculatorApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputReader reader = new InputReader();
+            double height = reader.GetHeight();
+            double weight = reader.GetWeight();
+
+            Calculate obj = new Calculate();
+            obj.SetHeight = height;
+            obj.SetWeight = weight;
+            double result = obj.calculate();
+            validate(result);
+
+
+        }
+    }
+}
