6b3c1e4307542ec6f269bdf130e726470f37a02e
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-26 22:08:14 +0530

    Add files via upload

---
+++ BMICalculatorApp/Calculate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorApp
+{
+    class Calculate
+    {
+        private double height, weight, result;
+        public double SetHeight
+        {
+            set
+            {
+                this.SetHeight = value;
+            }
+        }
+
+        public double SetWeight
+        {
+            set
+            {
+                this.SetWeight = value;
+            }
+        }
+        public double calculatebmi()
+        {
+            return result = weight / height;
+        }
+
+
+
+
+        public void validate(double Bmivalue)
+        {
+            if (Bmivalue < 18.5) { Console.WriteLine("Underweight"); }
+            else if (Bmivalue >= 25) { Console.WriteLine("Overweight"); }
+        }
+
+    }
+}
