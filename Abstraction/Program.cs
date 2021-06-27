7516bd1fd01889da27b95c4e4a0ec82139f663a7
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-27 20:31:11 +0530

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            TataCar _harrier = new TataCar(new VeriCoreEngine());
+            _harrier.Drive();
+            _harrier.Halt();
+            TataCar _nexon = new TataCar(new MultiEngine());
+            _nexon.Drive();
+            _nexon.Halt();
+            TataCar _hexa = new TataCar(new RevorTron());
+            _hexa.Drive();
+            _hexa.Halt();
+        }
+    }
+    
+}
---
+++ Abstraction/TataCar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class TataCar
+    {
+        
+           Iengine engine;
+           public TataCar(Iengine engine)
+           {
+            this.engine = engine;
+           }
+            public void Drive()
+            {
+
+                engine.Start();
+            }
+            public void Halt()
+            {
+
+                engine.Stop();
+            }
+
+    }
+    
+       
+}
+
