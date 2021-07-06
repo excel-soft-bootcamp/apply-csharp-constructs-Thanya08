9d0083ccd9dcd64d153cab255ed61ebab007a8f0
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-06 18:17:29 +0530

    Add files via upload

---
+++ BMIApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleInputReaderLib reader = new ConsoleInputReaderLib();
+            double height = reader.GetHeight();
+            double weight = reader.GetWeight();
+
+            CalculateLib obj = new CalculateLib(height, weight);
+            double Bmivalue = obj.BmiCalculation();
+
+            ValidationLib _validate = new ValidationLib();
+            string message = _validate.BmiValidate(Bmivalue);
+
+            ConsoleDisplayLib display = new ConsoleDisplayLib();
+            display.ConsoleDisplay(message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleInputReaderLib/InputReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInputReaderLib
+{
+    public class InputReader
+    {
+        public double GetHeight()
+        {
+            double height = double.Parse((this.ReadFromConsole("Enter height")));
+            return height;
+        }
+        public double GetWeight()
+        {
+            double weight = double.Parse((this.ReadFromConsole("Enter Weight")));
+            return weight;
+        }
+
+        private string ReadFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
