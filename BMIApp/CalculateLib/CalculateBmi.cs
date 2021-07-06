2b698da94bc3afd7a7939a9c89ba7e17d67021a1
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-06 22:23:29 +0530

    Add files via upload

---
+++ BMIApp/CalculateLib/CalculateBmi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLib
+{
+    public class CalculateBmi
+    {
+        private double height,weight,result;
+        public double Calculate(double height, double weight)
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public double calculatebmi()
+        {
+            return result = weight / height;
+        }
+
+
+    }
+}
