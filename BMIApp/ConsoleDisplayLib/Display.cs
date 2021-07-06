41d21db977943ae8f961e4324da2183eb6a8b3e7
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-06 22:25:31 +0530

    Add files via upload

---
+++ BMIApp/ConsoleDisplayLib/Display.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDisplayLib
+{
+    public class Display
+    {
+        public void ConsoleDisplay(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
