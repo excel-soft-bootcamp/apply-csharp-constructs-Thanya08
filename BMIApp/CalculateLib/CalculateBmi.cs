76091dc8bc3d00afe639fa7533e88eea09491e9c
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-06 22:50:47 +0530

    Update CalculateBmi.cs

--- BMIApp/CalculateLib/CalculateBmi.cs
+++ BMIApp/CalculateLib/CalculateBmi.cs
@@ -8,16 +8,10 @@ namespace CalculateLib
 {
     public class CalculateBmi
     {
-        private double height,weight,result;
-        public double Calculate(double height, double weight)
+      
+        public double calculatebmi(double height, double weight)
         {
-            this.height = height;
-            this.weight = weight;
-        }
-
-        public double calculatebmi()
-        {
-            return result = weight / height;
+            return double result = weight / height;
         }
 
 
