7ac3c565ee1fef88489775a5e44524104f9d8f47
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-09 19:10:45 +0530

    Add files via upload

---
+++ EventDrivenProgram/DoorSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDrivenProgram
+{
+    public delegate string DoorAction();
+    public class DoorSensor
+    {
+        public string open()
+        { 
+            return "door is open";
+           
+        }
+
+        public string close()
+        {
+            return "door is closed";
+        }
+    }
+}
---
+++ EventDrivenProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDrivenProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecuritySystem _observer = new SecuritySystem();
+            DoorSensor _sensor = new DoorSensor();
+            DoorAction _action_open = new DoorAction(_sensor.open);
+            DoorAction _action_close = new DoorAction(_sensor.close);
+            string open_message = _action_open.Invoke();
+            string close_message = _action_close.Invoke();
+            _observer.SendMessage(open_message);
+            _observer.SendMessage(close_message);
+        }
+    }
+}
